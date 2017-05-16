9983e571a3dcf430c630e6a15aeae80057baca7b
ChrisRackauckas <me@chrisrackauckas.com>
2017-05-16 02:23:04 -0700

    toy problems work

--- src/TensorFlowDiffEq.jl
+++ src/TensorFlowDiffEq.jl
@@ -7,7 +7,10 @@ import DiffEqBase: solve
 @compat abstract type TensorFlowAlgorithm <: AbstractODEAlgorithm end
 
 # DAE Algorithms
-immutable odetf <: TensorFlowAlgorithm end
+immutable odetf <: TensorFlowAlgorithm
+  hl_width::Int
+end
+odetf(;hl_width=64) = odetf(hl_width)
 
 export odetf
 
@@ -27,7 +30,6 @@ function solve(
     kwargs...)
 
     u0 = prob.u0
-    f = prob.f
     tspan = prob.tspan
 
     if dt == nothing
@@ -35,7 +37,8 @@ function solve(
     end
 
     sess=Session(Graph())
-    hl_width=64
+    hl_width=alg.hl_width
+    f = prob.f
 
 
     @tf begin #Automatically name nodes based on RHS
@@ -44,16 +47,16 @@ function solve(
 
         w1 = get_variable([1,hl_width], Float32)
         b1 = get_variable([hl_width], Float32)
-        w2 = get_variable([hl_width,1], Float32)
+        w2 = get_variable([hl_width,length(u0)], Float32)
 
-        u = tt.*nn.sigmoid(tt*w1 + b1)*w2
+        u = u0 + tt.*nn.sigmoid(tt*w1 + b1)*w2
 
 
         du_dt = gradients(u, tt)
         deq_rhs = f(tt,u) # - u/5 + exp(-tt/5).*cos(tt) # Should be f.(tt,u)
 
 
-        loss = reduce_sum((du_dt - deq_rhs).^2)
+        loss = reduce_mean((du_dt - deq_rhs).^2)
         opt = train.minimize(train.AdamOptimizer(), loss)
     end
 
@@ -70,7 +73,16 @@ function solve(
 
     u_net = run(sess, u, Dict(t=>t_obs))
 
-    build_solution(prob,alg,t_obs,u_net,
+    if typeof(u0) <: AbstractArray
+    timeseries = Vector{typeof(u0)}(0)
+    for i=1:size(u_net, 1)
+        push!(timeseries, reshape(view(u_net, i, :)', size(u0)))
+    end
+    else
+        timeseries = vec(u_net)
+    end
+
+    build_solution(prob,alg,t_obs,timeseries,
                dense = dense,
                timeseries_errors = timeseries_errors,
                retcode = :Success)
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,8 @@ using Base.Test
 using Plots; plotly()
 using DiffEqBase
 
+# Toy problem 1
+
 f = (t,u) -> -u/5 + exp(-t/5).*cos(t)
 (::typeof(f))(::Type{Val{:analytic}},t,u0) =  exp(-t/5)*(u0 + sin(t))
 prob = ODEProblem(f,0.0,(0.0,2.0))
@@ -11,6 +13,7 @@ sol = solve(prob,odetf(),dt=0.02)
 
 plot(sol,plot_analytic=true)
 
+# Toy problem 2
 
 function lorenz_tf(t,u)
     du1 = 10.0(u[:, 2]-u[:,1])
@@ -19,8 +22,30 @@ function lorenz_tf(t,u)
 
     [du1 du2 du3]
 end
-
-prob = ODEProblem(lorenz_tf,[1.0,1.0,0.0],(0.0,2.0))
+prob = ODEProblem(lorenz_tf,Float32[1.0,1.0,0.0],(0.0,100.0))
 sol = solve(prob,odetf(),dt=0.02)
 
-plot(sol,plot_analytic=true)
+plot(sol)
+
+# The real issue
+
+function lorenz(t,u)
+    du1 = 10.0(u[ 2]-u[1])
+    du2 = u[1].*(28.0-u[3]) - u[2]
+    du3 = u[1].*u[2] - (8/3)*u[3]
+
+    [du1 du2 du3]
+end
+
+
+function tf_function_wrapper(f,t,u)
+  du = similar(u)
+  for i in 1:size(u,1)
+    _u = view(i,:)
+    du[i,:] = f(t[i],_u)
+  end
+end
+
+wrapped_lorenz = (t,u) -> tf_function_wrapper(lorenz,t,u)
+prob = ODEProblem(wrapped_lorenz,Float32[1.0,1.0,0.0],(0.0,2.0))
+sol = solve(prob,odetf(),dt=0.02)
